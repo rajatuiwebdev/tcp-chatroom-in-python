c20f4d8cfd633ed6f6d2479d1fb1739a4ea34c12
Rajat Rajput <109746630+rajatuiwebdev@users.noreply.github.com>
2024-12-30 12:01:35 +0530

    base-files

---
+++ client.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+# Choosing Nickname
+nickname = input("Choose your name for the chat : ")
+
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 5500))
+# Listening to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            # If 'NAME'
+            message = client.recv(1024).decode('ascii')
+            if message == 'NAME':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            # Close Connection When Error
+            print("An error occured!")
+            client.close()
+            break
+# Sending Messages To Server
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
